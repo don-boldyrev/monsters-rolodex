5a2cdef09ccb7bc157c9df5c7e0298cabc412d72
Aleksandr Boldyrev <don.boldyrev2014@gmail.com>
2020-02-06 16:11:10 +0300

    Updates

---
+++ precache-manifest.84a4f304a947ccdab4b516599b255846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d115acbfeedb27524de68d33a49fe030",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "394fd52cb1eb81e7e5e9",
+    "url": "/monsters-rolodex/static/css/main.db95456e.chunk.css"
+  },
+  {
+    "revision": "ae4467ac1b6874332135",
+    "url": "/monsters-rolodex/static/js/2.f5a471c9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.f5a471c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394fd52cb1eb81e7e5e9",
+    "url": "/monsters-rolodex/static/js/main.189d018c.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
